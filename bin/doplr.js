b0b07c61260caafe55a451c49723d962be831670
erulabs <seandon.mooy@gmail.com>
2015-06-13 22:20:33 -0700

    a bit more koa

--- bin/doplr.js
+++ bin/doplr.js
@@ -1,59 +1,59 @@
 #! /usr/bin/env node
-"use strict";
+'use strict';
 
 // Doplr.js CLI entry point
 
 // Load lib/doplr
-const Doplr = require("./../lib");
-const CONSTANTS = require("./../lib/constants");
+const Doplr = require('./../lib');
+const CONSTANTS = require('./../lib/constants');
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 // https://github.com/bcoe/yargs
-const yargs = require("yargs")
-  .usage("Usage: $0 <command> [options]")
-  .count("verbose")
-  .alias("v", "verbose")
-  .describe("v", "verbosity, use twice for debug mode")
-  .alias("i", "ssh-key")
-  .describe("i", "ssh private key path")
-  .alias("u", "ssh-username")
-  .describe("u", "ssh username")
-  .default("u", process.env.USER)
-  .alias("p", "ssh-port")
-  .default("p", 22)
-  .describe("p", "ssh port")
-  .alias("c", "config")
-  .describe("c", "load configuration options from a .json file")
-  .alias("f", "forecast-path")
-  .default("f", ".")
-  .describe("f", "use a specific forecast directory")
-  .describe("s", "be silent")
-  .alias("s", "silence")
-  .default("s", false)
-  .alias("P", "radar-port")
-  .default("radar-port", CONSTANTS.DEFAULT_RADARPORT)
-  .describe("radar-port", "the port Doplr's API will bind to")
-  .alias("r", "radar")
-  .describe("radar", "target a remote radar")
-  .describe("no-weathergirl", "disable the web interface")
-  .command("sweep", "Discover a host, network, or cloud provider")
-  .command("forecast", "A CLI tool for browsing the forecast data")
-  .command("radar", "Run Doplr as a server")
-  .example("doplr sweep", "sweep all known infrastructure")
-  .example("doplr sweep host foo.com", "gather info about foo.com")
-  .example("doplr forecast", "display overview")
-  .example("doplr forecast host foo.com", "display info about foo.com")
-  .example("doplr radar", "launch the web interface")
-  .example("doplr <command> --radar=radar.foo.com", "Run against a remote radar")
-  .example("node ./bin/radar.js", "run your own radar server")
-  .help("h")
-  .alias("h", "help")
-  .epilog("Visit https://github.com/asdqwex/doplr for more information!");
+const yargs = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .count('verbose')
+  .alias('v', 'verbose')
+  .describe('v', 'verbosity, use twice for debug mode')
+  .alias('i', 'ssh-key')
+  .describe('i', 'ssh private key path')
+  .alias('u', 'ssh-username')
+  .describe('u', 'ssh username')
+  .default('u', process.env.USER)
+  .alias('p', 'ssh-port')
+  .default('p', 22)
+  .describe('p', 'ssh port')
+  .alias('c', 'config')
+  .describe('c', 'load configuration options from a .json file')
+  .alias('f', 'forecast-path')
+  .default('f', '.')
+  .describe('f', 'use a specific forecast directory')
+  .describe('s', 'be silent')
+  .alias('s', 'silence')
+  .default('s', false)
+  .alias('P', 'radar-port')
+  .default('radar-port', CONSTANTS.DEFAULT_RADARPORT)
+  .describe('radar-port', `the port Doplr's API will bind to`)
+  .alias('r', 'radar')
+  .describe('radar', 'target a remote radar')
+  .describe('no-weathergirl', 'disable the web interface')
+  .command('sweep', 'Discover a host, network, or cloud provider')
+  .command('forecast', 'A CLI tool for browsing the forecast data')
+  .command('radar', 'Run Doplr as a server')
+  .example('doplr sweep', 'sweep all known infrastructure')
+  .example('doplr sweep host foo.com', 'gather info about foo.com')
+  .example('doplr forecast', 'display overview')
+  .example('doplr forecast host foo.com', 'display info about foo.com')
+  .example('doplr radar', 'launch the web interface')
+  .example('doplr <command> --radar=radar.foo.com', 'Run against a remote radar')
+  .example('node ./bin/radar.js', 'run your own radar server')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Visit https://github.com/asdqwex/doplr for more information!');
 
 const argv = yargs.argv;
-const log = require("./../lib/logger")(argv);
+const log = require('./../lib/logger')(argv);
 
 if (argv._.length === 0) {
   log(yargs.help());
@@ -74,18 +74,18 @@ function locateForecast (p) {
   return false;
 }
 
-// The first non-hypenated option is the "action"
+// The first non-hypenated option is the 'action'
 const chosenAction = argv._[0];
 argv._.shift();
 
 // `doplr <action> --radar` implies a user wants to send this task to a radar daemon
 if (argv.radar) {
   let radarUri = argv.radar;
-  if (typeof radarUri !== "string") {
+  if (typeof radarUri !== 'string') {
     radarUri = CONSTANTS.DEFAULT_RADARURI;
   }
   // Send messages to DAEMONs via HTTP (HTTPS eventually)
-  //const http = require("http");
+  //const http = require('http');
   // POST /targetUri { data: options } ...
   log(CONSTANTS.UNIMPLIMENTED);
 
@@ -95,7 +95,7 @@ if (argv.radar) {
   const doplr = new Doplr({
     // Locate the nearest .forecast - If none is found, we'll create one where we are
     targetForecast: locateForecast(
-        path.resolve(argv["forecast-path"].replace(CONSTANTS.DEFAULT_FORECAST_DIRECTORY, ""))
+        path.resolve(argv['forecast-path'].replace(CONSTANTS.DEFAULT_FORECAST_DIRECTORY, ''))
       ) || CONSTANTS.DEFAULT_FORECAST_DIRECTORY,
     verbose: argv.verbose,
     silent: argv.silent
@@ -103,30 +103,30 @@ if (argv.radar) {
   log.info(`Forecast directory: ${doplr.forecastPath}`);
 
   // `doplr radar` Launch the API
-  if (["r", "ra", "rad", "rada", "radar"].indexOf(chosenAction) > -1) {
+  if (['r', 'ra', 'rad', 'rada', 'radar'].indexOf(chosenAction) > -1) {
     const radar = new doplr.Radar({
-      weathergirl: !argv["no-weathergirl"]
+      weathergirl: !argv['no-weathergirl']
     });
-    radar.listen(argv["radar-port"]);
+    radar.listen(argv['radar-port']);
 
     // Pop the browser if we're hosting weathergirl
-    if (!argv["no-weathergirl"] &&
+    if (!argv['no-weathergirl'] &&
     !argv.silent && // And we're not silent
-    typeof argv["radar-port"] === "number" && // And radar is listening on a TCP port
-    process.platform !== "linux") { // And we're not on linux (shrug)
+    typeof argv['radar-port'] === 'number' && // And radar is listening on a TCP port
+    process.platform !== 'linux') { // And we're not on linux (shrug)
       setTimeout(function () {
-        const open = require("open");
-        open("http://localhost:" + argv["radar-port"]);
+        const open = require('open');
+        open('http://localhost:' + argv['radar-port']);
       }, 100);
     }
 
   // `doplr sweep <type> <target> [options]`
-  } else if (["s", "sw", "swe", "swee", "sweep"].indexOf(chosenAction) > -1) {
+  } else if (['s', 'sw', 'swe', 'swee', 'sweep'].indexOf(chosenAction) > -1) {
     if (argv._.length === 0) {
-      log("Usage: doplr sweep <host|network|cloud_provider|plugin_name> [options]\n",
-          "Example: \n\t",
-          "doplr sweep host myserver.com\n\t",
-          "doplr sweep aws AWS_SECRET_ID AWS_SECRET_KEY\n",
+      log('Usage: doplr sweep <host|network|cloud_provider|plugin_name> [options]\n',
+          'Example: \n\t',
+          'doplr sweep host myserver.com\n\t',
+          'doplr sweep aws AWS_SECRET_ID AWS_SECRET_KEY\n',
           yargs.help());
       process.exit(1);
     }
@@ -136,10 +136,10 @@ if (argv.radar) {
 
     // Validate arguments for different sweep types
     // Host
-    if (["h", "ho", "hos", "host"].indexOf(sweepType) > -1) {
+    if (['h', 'ho', 'hos', 'host'].indexOf(sweepType) > -1) {
       sweep.type = CONSTANTS.HOST;
       if (argv._.length === 0) {
-        log("You must provide a host to sweep!\n",
+        log('You must provide a host to sweep!\n',
           yargs.help());
         process.exit(1);
       }
@@ -148,25 +148,25 @@ if (argv.radar) {
       // Pass more options here...
 
     // Network
-    } else if (["n", "ne", "net", "netw", "netwo", "networ", "network"].indexOf(sweepType) > -1) {
+    } else if (['n', 'ne', 'net', 'netw', 'netwo', 'networ', 'network'].indexOf(sweepType) > -1) {
       sweep.type = CONSTANTS.NETWORK;
       // Validate arguments here
       log(CONSTANTS.UNIMPLIMENTED);
 
     // AWS
-    } else if (["aws", "ec2", "amazon"].indexOf(sweepType) > -1) {
+    } else if (['aws', 'ec2', 'amazon'].indexOf(sweepType) > -1) {
       sweep.type = CONSTANTS.AWS;
       // Validate arguments here
       log(CONSTANTS.UNIMPLIMENTED);
 
     // Google Cloud
-    } else if (["google", "gce", "goog", "gc"].indexOf(sweepType) > -1) {
+    } else if (['google', 'gce', 'goog', 'gc'].indexOf(sweepType) > -1) {
       sweep.type = CONSTANTS.GOOGLE_CLOUD;
       // Validate arguments here
       log(CONSTANTS.UNIMPLIMENTED);
 
     // OpenStack
-    } else if (["openstack"].indexOf(sweepType) > -1) {
+    } else if (['openstack'].indexOf(sweepType) > -1) {
       sweep.type = CONSTANTS.OPENSTACK;
       // Validate arguments here
       log(CONSTANTS.UNIMPLIMENTED);
@@ -175,7 +175,7 @@ if (argv.radar) {
       // Assuming the TYPE is not a host, network or provider for which
       // we have customer arg parsing, we should do two things:
       // 1. We should check if we have a plugin loaded which matches this type
-      if (typeof sweep.types[sweepType] === "string") {
+      if (typeof sweep.types[sweepType] === 'string') {
         sweepType = sweep.types[sweepType];
       }
       if (sweep.types[sweepType] === undefined) {
@@ -190,8 +190,8 @@ if (argv.radar) {
     sweep.begin();
 
   // `doplr forecast <categoryOrKeyword> [options]`
-  } else if (["f", "fo", "for", "fore", "forec",
-              "foreca", "forecas", "forecast"].indexOf(chosenAction) > -1) {
+  } else if (['f', 'fo', 'for', 'fore', 'forec',
+              'foreca', 'forecas', 'forecast'].indexOf(chosenAction) > -1) {
     const forecast = new doplr.Forecast({});
     forecast.report();
 
--- bin/radar.js
+++ bin/radar.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-const Doplr = require("./../lib");
-const CONSTANTS = require("./../lib/constants");
+const Doplr = require('./../lib');
+const CONSTANTS = require('./../lib/constants');
 
 const doplr = new Doplr({
   // Locate the nearest .forecast - If none is found, we'll create one where we are
-  targetForecast: process.env.FORCAST_PATH || ".",
+  targetForecast: process.env.FORCAST_PATH || '.',
   verbose: process.env.VERBOSE || 0,
   silent: process.env.SILENT || false
 });
