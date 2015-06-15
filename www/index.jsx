04a10cabf35ccf1f3c5c11ea826d7cb542eb05ba
erulabs <seandon.mooy@gmail.com>
2015-06-14 20:49:28 -0700

    add d3, more UI stuff

--- www/index.jsx
+++ www/index.jsx
@@ -8,4 +8,16 @@ const Hello = React.createClass({
     }
 });
 
-React.render(<Hello name="World2" />, document.getElementById('container'));
+//React.render(<Hello name="World2" />, document.getElementById('container'));
+
+const svg = d3.select('svg');
+
+var circle = svg.selectAll('circle')
+    .data([32, 57, 112, 293]);
+
+var circleEnter = circle.enter().append('circle');
+
+circleEnter.style("fill", "steelblue");
+circleEnter.attr("cy", 60);
+circleEnter.attr("cx", function(d, i) { return i * 100 + 300; });
+circleEnter.attr("r", function(d) { return Math.sqrt(d); });
